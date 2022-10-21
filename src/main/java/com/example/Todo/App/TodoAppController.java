39056cdb5260a73ff5e99de3139f3a2a3e159ff1
kwang <hea0408never@naver.com>
2022-10-21 15:49:53 +0900

    dev

---
+++ src/main/java/com/example/Todo/App/TodoAppController.java
@@ -0,0 +1,13 @@
+package com.example.Todo.App;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoAppController {
+
+    @GetMapping("/")
+    public String todoapplication() {
+        return "To-do Application";
+    }
+}
